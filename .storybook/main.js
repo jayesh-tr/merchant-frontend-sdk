7d0a2fca657833c6d008dae6342fd6ca1e0a66dc
Jayesh jawale <jjawale@techracers.io>
2020-05-21 17:08:05 +0530

    configured accessibility add on for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -23,5 +23,6 @@ module.exports = {
         },
       },
     },
+    '@storybook/addon-a11y'
   ],
 };
--- src/stories/2-footer.stories.ts
+++ src/stories/2-footer.stories.ts
@@ -1,9 +1,11 @@
 import { Welcome } from '@storybook/angular/demo';
+import { withA11y } from '@storybook/addon-a11y';
 import { FooterComponent } from '../../projects/ng9-business-console/src/lib/footer/footer.component'
 
 export default {
   title: 'Welcome',
   component: FooterComponent,
+  decorators: [withA11y]
 };
 
 export const ToStorybook = () => ({
