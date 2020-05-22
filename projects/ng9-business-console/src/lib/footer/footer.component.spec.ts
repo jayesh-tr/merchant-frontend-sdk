70d242da4732a6c9db2c3222051d313a327611fb
Jayesh jawale <jjawale@techracers.io>
2020-05-22 20:27:26 +0530

    Configured jest framework for the project

--- projects/ng9-business-console/src/lib/footer/footer.component.spec.ts
+++ projects/ng9-business-console/src/lib/footer/footer.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+// import { FooterComponent } from './footer.component';
+
+// describe('FooterComponent', () => {
+//   let component: FooterComponent;
+//   let fixture: ComponentFixture<FooterComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ FooterComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(FooterComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+import { render } from '@testing-library/angular';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render the component', async () => {
+    const { getByText } = await render(FooterComponent);
+    expect(getByText('footer component works!'));
   });
 });
