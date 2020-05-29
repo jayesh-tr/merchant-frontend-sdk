1517bfc1ecd942398dab39dbeda0e7f4b5a43ce8
Jayesh jawale <jjawale@techracers.io>
2020-05-29 20:17:07 +0530

    Fixed schematic build

--- projects/ng9-business-console/src/public-api.ts
+++ projects/ng9-business-console/src/public-api.ts
@@ -2,15 +2,21 @@
  * Public API Surface of ng9-business-console
  */
 
+// modules
 export * from './lib/ng9-business-console.module';
 export * from './lib/components/product-table/product-table.module';
 export * from './lib/components/footer/footer.module';
+
 // components
 export * from './lib/components/footer/footer.component';
 export * from './lib/components/top-nav/top-nav.component';
 export * from './lib/components/product-table/product-table.component';
+
+// services
+export * from './lib/services/authentication/authentication.service';
 export * from './lib/services/product/product.service';
 export * from './lib/services/footer/footer.service';
-export * from './lib/services/authentication/authentication.service';
+
+// models
 export * from './lib/models/authentication-model';
 export * from './lib/models/product-model';
